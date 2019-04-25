5a9965fccbc379bc7ab561c7c2a56ba427c2aff5
Frozii <ramireini@gmail.com>
2019-04-26 00:51:54 +0300

    big fixes

--- src/npong.c
+++ src/npong.c
@@ -1,7 +1,7 @@
 #include <npong.h>
 
 int running = 1;
-int gamestate = 0;
+int gamestate = gamestate_main_menu;
 
 int game_win_start_x = 0;
 int game_win_start_y = 3;
@@ -40,20 +40,52 @@ void init_renderer_and_options()
   // make the cursor invisible
   curs_set(0);
 
-  // background color
-  init_color(COLOR_CYAN, 0, 200, 250);
-  init_pair(1, COLOR_WHITE, COLOR_CYAN);
-
-  // selected text color
+  init_color(COLOR_BLACK, 0, 0, 0);
+  init_color(COLOR_RED, 1000, 0, 0);
   init_color(COLOR_GREEN, 420, 600, 140);
-  init_pair(2, COLOR_GREEN, COLOR_CYAN);
-
-  // non-selected text color
+  init_color(COLOR_YELLOW, 1000, 1000, 0);
   init_color(COLOR_BLUE, 130, 490, 720);
-  init_pair(3, COLOR_BLUE, COLOR_CYAN);
+  init_color(COLOR_MAGENTA, 1000, 0, 1000);
+  init_color(COLOR_CYAN, 0, 200, 250);
+  init_color(COLOR_WHITE, 1000, 1000, 1000);
+
+  init_pair(black_pair, COLOR_BLACK, COLOR_BLACK);
+  init_pair(red_pair, COLOR_RED, COLOR_CYAN);
+  init_pair(green_pair, COLOR_GREEN, COLOR_CYAN);
+  init_pair(yellow_pair, COLOR_YELLOW, COLOR_CYAN);
+  init_pair(blue_pair, COLOR_BLUE, COLOR_CYAN);
+  init_pair(magenta_pair, COLOR_MAGENTA, COLOR_CYAN);
+  init_pair(cyan_pair, COLOR_CYAN, COLOR_CYAN);
+  init_pair(white_pair, COLOR_WHITE, COLOR_CYAN);
 }
 
-int update_and_draw_ball(WINDOW *game_win, ball_t *ball, player_t *player_one, player_t *player_two)
+void render_ball(WINDOW *game_win, ball_t *ball)
+{
+  wattron(game_win, COLOR_PAIR(white_pair));
+  mvwprintw(game_win, ball->y, ball->x, "%c", ball->symbol);
+  wattroff(game_win, COLOR_PAIR(white_pair));
+}
+
+void render_players(WINDOW *game_win, player_t *player_one, player_t *player_two)
+{
+  wattron(game_win, COLOR_PAIR(white_pair));
+
+  // player one
+  for (int y = 0; y < player_one->height; y++)
+  {
+    mvwaddstr(game_win, player_one->y + y, player_one->x, "|");
+  }
+  
+  // player two
+  for (int y = 0; y < player_two->height; y++)
+  {
+    mvwaddstr(game_win, player_two->y + y, player_two->x, "|");
+  }
+
+  wattroff(game_win, COLOR_PAIR(white_pair));
+}
+
+int update_ball(ball_t *ball, player_t *player_one, player_t *player_two)
 {
   /*
    * Ball->flag_x:
@@ -67,48 +99,62 @@ int update_and_draw_ball(WINDOW *game_win, ball_t *ball, player_t *player_one, p
 
   // move the ball
   if (ball->flag_x == 0)
+  {
     ball->x -= ball->vx;
+  }
   else if (ball->flag_x == 1)
+  {
     ball->x += ball->vx;
+  }
 
   if (ball->flag_y == 0)
+  {
     ball->y -= ball->vy;
+  }
   else if (ball->flag_y == 1)
+  {
     ball->y += ball->vy;
+  }
 
-  // check if the ball is past the screen on the x plane
+  // check if the ball is past the screen on the x axis
   if (ball->x < 0)
   {
-    player_two->score += 1;
+    player_two->score++;
+
+    if (player_two->score >= 5)
+    {
+      winner = 2;
+    }
+
     ball->flag_x = rand() % 2;
     ball->flag_y = rand() % 2;
     ball->x = game_win_width / 2;
     ball->y = game_win_height / 2;
-    ball->vx = 0.002;
-    ball->vy = 0.002;
+    ball->vx = 0.0002;
+    ball->vy = 0.0002;
     player_one->can_add_speed = 1;
     player_two->can_add_speed = 1;
-
-    if (player_two->score >= 5)
-      winner = 2;
   }
   else if (ball->x > game_win_width)
   {
-    player_one->score += 1;
+    player_one->score++;
+
+    if (player_one->score >= 5)
+    {
+      winner = 1;
+    }
+
     ball->flag_x = rand() % 2;
     ball->flag_y = rand() % 2;
     ball->x = game_win_width / 2;
     ball->y = game_win_height / 2;
-    ball->vx = 0.002;
-    ball->vy = 0.002;
+    ball->vx = 0.0002;
+    ball->vy = 0.0002;
     player_one->can_add_speed = 1;
     player_two->can_add_speed = 1;
-
-    if (player_one->score >= 5)
-      winner = 1;
   }
 
-  // check if the ball is past the screen on the y plane and reverse the flag
+  // check if the ball is past the screen on the y axis
   if (ball->y < 1)
   {
     ball->flag_y = 1;
@@ -134,10 +180,10 @@ int update_and_draw_ball(WINDOW *game_win, ball_t *ball, player_t *player_one, p
     if (player_one->can_add_speed == 1)
     {
       // add speed to the ball if it's below the max
-      if (ball->vx && ball->vy < 0.008)
+      if (ball->vx && ball->vy < 0.002)
       {
-        ball->vx += 0.0004;
-        ball->vy += 0.0004;
+        ball->vx += 0.0001;
+        ball->vy += 0.0001;
       }
 
       player_one->can_add_speed = 0;
@@ -157,10 +203,10 @@ int update_and_draw_ball(WINDOW *game_win, ball_t *ball, player_t *player_one, p
     if (player_two->can_add_speed == 1)
     {
       // add speed to the ball if it's below the max
-      if (ball->vx && ball->vy < 0.008)
+      if (ball->vx && ball->vy < 0.002)
       {
-        ball->vx += 0.0004;
-        ball->vy += 0.0004;
+        ball->vx += 0.0001;
+        ball->vy += 0.0001;
       }
 
       player_one->can_add_speed = 1;
@@ -168,16 +214,14 @@ int update_and_draw_ball(WINDOW *game_win, ball_t *ball, player_t *player_one, p
     }
   }
 
-  mvwprintw(game_win, ball->y, ball->x, "%c", ball->symbol);
-
   return winner;
 }
 
-int draw_and_update_menu(WINDOW *game_win)
+void render_main_menu(WINDOW *game_win)
 {
   // calculate the amount to print for the title text
   int current_width = getmaxx(game_win);
-  int amount_to_remove;
+  int amount_to_remove = 0;
   int amount_to_print = 44;
 
   if (current_width < 52)
@@ -187,12 +231,14 @@ int draw_and_update_menu(WINDOW *game_win)
   }
 
   // draw game title
+  wattron(game_win, COLOR_PAIR(white_pair));
   mvwaddnstr(game_win, 1, 8, " _   _   _____   _____   _   _   _____ ", amount_to_print);
   mvwaddnstr(game_win, 2, 8, "/ \\ / \\ /  __ \\ /  _  \\ / \\ / \\ /  __ \\", amount_to_print);
   mvwaddnstr(game_win, 3, 8, "|  \\| | | /_/ | | | | | |  \\  | | |  \\/", amount_to_print);
   mvwaddnstr(game_win, 4, 8, "| . ` | |  __/  | | | | | . ` | | | ___", amount_to_print);
   mvwaddnstr(game_win, 5, 8, "| |\\  | | |     | |_| | | |\\  | | |_| |", amount_to_print);
   mvwaddnstr(game_win, 6, 8, "\\_/ \\_/ \\_/     \\_____/ \\_/ \\_/ \\_____/", amount_to_print);
+  wattroff(game_win, COLOR_PAIR(white_pair));
 
   // draw the menu options
   char menu_item[7] = "> ";
@@ -211,49 +257,52 @@ int draw_and_update_menu(WINDOW *game_win)
     {
       strcpy(menu_item + 2, menu_items[i]);
 
-      wattron(game_win, COLOR_PAIR(2));
+      wattron(game_win, COLOR_PAIR(blue_pair));
       mvwaddnstr(game_win, 10, 6, menu_item, amount_to_print);
-      wattroff(game_win, COLOR_PAIR(2));
+      wattroff(game_win, COLOR_PAIR(blue_pair));
 
-      wattron(game_win, COLOR_PAIR(3));
+      wattron(game_win, COLOR_PAIR(white_pair));
       mvwaddnstr(game_win, 11, 8, menu_items[1], amount_to_print - 2);
 
       mvwaddnstr(game_win, 12, 8, menu_items[2], amount_to_print - 2);
-      wattroff(game_win, COLOR_PAIR(3));
+      wattroff(game_win, COLOR_PAIR(white_pair));
     }
     else if (i == highlighted && highlighted == 1)
     {
       strcpy(menu_item + 2, menu_items[i]);
 
-      wattron(game_win, COLOR_PAIR(3));
+      wattron(game_win, COLOR_PAIR(white_pair));
       mvwaddnstr(game_win, 10, 8, menu_items[0], amount_to_print - 2);
-      wattroff(game_win, COLOR_PAIR(3));
+      wattroff(game_win, COLOR_PAIR(white_pair));
 
-      wattron(game_win, COLOR_PAIR(2));
+      wattron(game_win, COLOR_PAIR(blue_pair));
       mvwaddnstr(game_win, 11, 6, menu_item, amount_to_print);
-      wattroff(game_win, COLOR_PAIR(2));
+      wattroff(game_win, COLOR_PAIR(blue_pair));
 
-      wattron(game_win, COLOR_PAIR(3));
+      wattron(game_win, COLOR_PAIR(white_pair));
       mvwaddnstr(game_win, 12, 8, menu_items[2], amount_to_print - 2);
-      wattroff(game_win, COLOR_PAIR(3));
+      wattroff(game_win, COLOR_PAIR(white_pair));
     }
     else if (i == highlighted && highlighted == 2)
     {
       strcpy(menu_item + 2, menu_items[i]);
 
-      wattron(game_win, COLOR_PAIR(3));
+      wattron(game_win, COLOR_PAIR(white_pair));
       mvwaddnstr(game_win, 10, 8, menu_items[0], amount_to_print - 2);
 
       mvwaddnstr(game_win, 11, 8, menu_items[1], amount_to_print - 2);
-      wattroff(game_win, COLOR_PAIR(3));
+      wattroff(game_win, COLOR_PAIR(white_pair));
 
-      wattron(game_win, COLOR_PAIR(2));
+      wattron(game_win, COLOR_PAIR(blue_pair));
       mvwaddnstr(game_win, 12, 6, menu_item, amount_to_print);
-      wattroff(game_win, COLOR_PAIR(2));
+      wattroff(game_win, COLOR_PAIR(blue_pair));
     }
   }
+}
 
-  // record user menu movement between menu items
+int update_main_menu(WINDOW *game_win)
+{
+  // record user movement between menu items
   menu_choice = wgetch(game_win);
   switch (menu_choice)
   {
@@ -300,9 +349,10 @@ int draw_and_update_menu(WINDOW *game_win)
   return 4;
 }
 
-void draw_help(WINDOW *game_win)
+void render_help(WINDOW *game_win)
 {
   // draw help text
+  wattron(game_win, COLOR_PAIR(white_pair));
   mvwaddstr(game_win, 1, 8, " _   _   _____   _       _____");
   mvwaddstr(game_win, 2, 8, "| | | | |  ___| | |     /  __ \\");
   mvwaddstr(game_win, 3, 8, "| |_| | | |__   | |     | |_/ /");
@@ -315,18 +365,23 @@ void draw_help(WINDOW *game_win)
   mvwaddstr(game_win, 13, 8, "Player 2: up and down using arrow keys");
   mvwaddstr(game_win, 15, 8, "You can press 'q' to exit the game if needed ingame");
   mvwaddstr(game_win, 17, 8, "First player to score 5 points wins");
+  wattroff(game_win, COLOR_PAIR(white_pair));
 }
 
-void draw_score_and_window(WINDOW *score_win, int *player_one_score, int *player_two_score)
+void render_score_window(WINDOW *score_win, int player_one_score, int player_two_score)
 {
+  wattron(score_win, COLOR_PAIR(white_pair));
+
   // resize window to keep it in place
   wresize(score_win, score_win_height, score_win_width);
 
   // draw box border around the window
   box(score_win, 0, 0);
 
-  mvwprintw(score_win, 1, 1, "Player 1: %d", *player_one_score);
-  mvwprintw(score_win, 1, 48, "Player 2: %d", *player_two_score);
+  mvwprintw(score_win, 1, 1, "Player 1: %d", player_one_score);
+  mvwprintw(score_win, 1, 48, "Player 2: %d", player_two_score);
+
+  wattroff(score_win, COLOR_PAIR(white_pair));
 }
 
 void update_players(player_t *player_one, player_t *player_two)
@@ -377,9 +432,10 @@ void update_players(player_t *player_one, player_t *player_two)
   }
 }
 
-void draw_players_and_arena(WINDOW *game_win, player_t *player_one, player_t *player_two)
+void render_arena(WINDOW *game_win)
 {
   // corners
+  wattron(game_win, COLOR_PAIR(white_pair));
   mvwaddstr(game_win, 0, 0, "+");
   mvwaddstr(game_win, 0, game_win_width - 1, "+");
   mvwaddstr(game_win, game_win_height - 1, 0, "+");
@@ -399,17 +455,7 @@ void draw_players_and_arena(WINDOW *game_win, player_t *player_one, player_t *pl
     mvwaddstr(game_win, game_win_height - 1, x, "-");
   }
 
-  // draw player one
-  for (int y = 0; y < player_one->height; y++)
-  {
-    mvwaddstr(game_win, player_one->y + y, player_one->x, "|");
-  }
-  
-  // draw player two
-  for (int y = 0; y < player_two->height; y++)
-  {
-    mvwaddstr(game_win, player_two->y + y, player_two->x, "|");
-  }
+  wattroff(game_win, COLOR_PAIR(white_pair));
 }
 
 void set_window_options(WINDOW *game_win, WINDOW *score_win)
@@ -418,9 +464,9 @@ void set_window_options(WINDOW *game_win, WINDOW *score_win)
   keypad(game_win, TRUE);
 
   // change the bg colors for the windows
-  wbkgd(stdscr, COLOR_PAIR(1));
-  wbkgd(game_win, COLOR_PAIR(1));
-  wbkgd(score_win, COLOR_PAIR(1));
+  wbkgd(stdscr, COLOR_PAIR(cyan_pair));
+  wbkgd(game_win, COLOR_PAIR(cyan_pair));
+  wbkgd(score_win, COLOR_PAIR(cyan_pair));
 
   // refresh all the windows
   wrefresh(stdscr);
@@ -457,93 +503,62 @@ ball_t* create_ball(char symbol, int x, int y, float vx, float vy)
   return b;
 }
 
-void victory_screen(WINDOW *game_win, WINDOW *score_win, player_t *player_one, player_t *player_two, int winner)
+// NOTE(Rami): 
+void render_victory_screen(WINDOW *game_win, WINDOW *score_win, int winner)
 {
-  while (winner == 1 || winner == 2)
+  nodelay(game_win, FALSE);
+
+  werase(game_win);
+  werase(score_win);
+
+  wattron(game_win, COLOR_PAIR(white_pair));
+  mvwprintw(game_win, 5, game_win_width / 2, "Player %d wins!", winner);
+  mvwaddstr(game_win, 10, 26, "[Q] for main menu");
+  mvwaddstr(game_win, 11, 26, "[R] for a rematch");
+
+  for (int y = 3; y < 8; y++)
   {
-    wclear(game_win);
-    wclear(score_win);
+    mvwaddstr(game_win, y, (game_win_width / 2) - 4, "|");
+    mvwaddstr(game_win, y, (game_win_width / 2) + 17, "|");
+  }
 
-    mvwprintw(game_win, 5, game_win_width / 2, "Player %d wins!", winner);
-    mvwaddstr(game_win, 10, 26, "[Q] for main menu");
-    mvwaddstr(game_win, 11, 26, "[R] for a rematch");
+  for (int x = 26; x < 48; x++)
+  {
+    mvwaddstr(game_win, 2, x, "-");
+    mvwaddstr(game_win, 8, x, "-");
+  }
 
-    for (int y = 3; y < 8; y++)
+  wattroff(game_win, COLOR_PAIR(white_pair));
+  wrefresh(game_win);
+  wrefresh(score_win);
+
+  while (1)
+  {
+    int input = wgetch(game_win);
+
+    if (input == 'Q' || input == 'q')
     {
-      mvwaddstr(game_win, y, (game_win_width / 2) - 4, "|");
-      mvwaddstr(game_win, y, (game_win_width / 2) + 17, "|");
-    }
-
-    for (int x = 26; x < 48; x++)
-    {
-      mvwaddstr(game_win, 2, x, "-");
-      mvwaddstr(game_win, 8, x, "-");
-    }
-
-    int choice = wgetch(game_win);
-
-    switch (choice)
-    {
-      case 'Q':
-      case 'q':
-        // reset score
-        player_one->score = 0;
-        player_two->score = 0;
-
-        // reset position
-        player_one->y = (game_win_height / 2) - 2;
-        player_two->y = (game_win_height / 2) - 2;
-
-        // switch gamestate
-        gamestate = 0;
-
-        // turn off nodelay
-        nodelay(game_win, FALSE);
-
-        // break out of loop
-        winner = 0;
-        break;
-
-      case 'R':
-      case 'r':
-        // reset score
-        player_one->score = 0;
-        player_two->score = 0;
-
-        // reset position
-        player_one->y = (game_win_height / 2) - 2;
-        player_two->y = (game_win_height / 2) - 2;
-
-        // break out of loop
-        winner = 0;
-        break;
-
-      default:
+      gamestate = gamestate_main_menu;
       break;
     }
-
-    wrefresh(game_win);
-    wrefresh(score_win);
+    else if (input == 'R' || input == 'r')
+    {
+      nodelay(game_win, TRUE);
+      break;
+    }
   }
 }
 
-void free_player(player_t *p)
+void clean_up(WINDOW *game_win, WINDOW *score_win, ball_t *ball, player_t *player_one, player_t *player_two)
 {
-  free(p);
-  p = NULL;
-}
+  free(player_one);
+  player_one = NULL;
 
-void free_ball(ball_t *b)
-{
-  free(b);
-  b = NULL;
-}
+  free(player_two);
+  player_two = NULL;
 
-void clean_up(WINDOW *game_win, WINDOW *score_win, player_t *p_one, player_t *p_two, ball_t *b)
-{
-  free_player(p_one);
-  free_player(p_two);
-  free_ball(b);
+  free(ball);
+  ball = NULL;
 
   // delete game window
   delwin(game_win);
