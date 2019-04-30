353fddb13a16f5ae234fd0f3e1299be1def4dc23
Rami Reini <ramireini@gmail.com>
2019-04-30 21:14:47 +0300

    stuff

--- src/linux_npong.c
+++ src/linux_npong.c
@@ -21,6 +21,11 @@ typedef double r64;
 
 typedef u32 b32;
 
+// NOTE(Rami): Do the same resize trick to render_controls()
+// NOTE(Rami): If you shrink the window enough and then expand,
+// there won't be anything rendered but the background, can we detect and
+// fix this?
+
 enum
 {
   black_pair,
@@ -35,19 +40,25 @@ enum
   cyan_background_pair
 };
 
-enum
+typedef enum
 {
-  gamestate_main_menu,
-  gamestate_play,
-  gamestate_controls,
-};
+  state_main_menu,
+  state_play,
+  state_controls,
+  state_quit
+} game_state_e;
+
+typedef struct
+{
+  game_state_e state;
+} game_t;
 
 typedef struct
 {
   i32 x;
   i32 y;
-  i32 width;
-  i32 height;
+  i32 w;
+  i32 h;
   i32 score;
   i32 can_add_speed;
 } player_t;
@@ -64,167 +75,173 @@ typedef struct
   r32 vy;
 } ball_t;
 
-i32 running = 1;
-i32 gamestate = gamestate_main_menu;
+i32 game_win_x;
+i32 game_win_y = 3;
+i32 game_win_w = 60;
+i32 game_win_h = 20;
 
-i32 game_win_start_x = 0;
-i32 game_win_start_y = 3;
-i32 game_win_width = 60;
-i32 game_win_height = 20;
+i32 score_win_w = 60;
+i32 score_win_h = 3;
 
-i32 score_win_width = 60;
-i32 score_win_height = 3;
-
-i32 key_pressed = 0;
+i32 key_pressed;
 i32 menu_choice;
-i32 highlighted = 0;
+i32 highlighted;
 i32 menu_item_amount = 3;
 char *menu_items[] = {"Play", "Controls", "Quit"};
 
+global game_t game;
+
+global WINDOW *game_win;
+global WINDOW *score_win;
+
+global player_t player_one;
+global player_t player_two;
+global ball_t ball;
+
 internal void
-render_ball(WINDOW *game_win, ball_t *ball)
+render_ball()
 {
   wattron(game_win, COLOR_PAIR(white_pair));
-  mvwprintw(game_win, ball->y, ball->x, "%c", ball->glyph);
+  mvwprintw(game_win, ball.y, ball.x, "%c", ball.glyph);
   wattroff(game_win, COLOR_PAIR(white_pair));
 }
 
 internal void
-render_players(WINDOW *game_win, player_t *player_one, player_t *player_two)
+render_players()
 {
   wattron(game_win, COLOR_PAIR(white_pair));
-  for(i32 y = 0; y < player_one->height; y++) {mvwaddstr(game_win, player_one->y + y, player_one->x, "|");}
-  for(i32 y = 0; y < player_two->height; y++) {mvwaddstr(game_win, player_two->y + y, player_two->x, "|");}
+  for(i32 y = 0; y < player_one.h; y++) {mvwaddstr(game_win, player_one.y + y, player_one.x, "|");}
+  for(i32 y = 0; y < player_two.h; y++) {mvwaddstr(game_win, player_two.y + y, player_two.x, "|");}
   wattroff(game_win, COLOR_PAIR(white_pair));
 }
 
 internal i32
-update_ball(ball_t *ball, player_t *player_one, player_t *player_two)
+update_ball()
 {
   /*
-   * Ball->flag_x:
+   * ball.flag_x:
    * 0 = left, 1 = right
    *
-   * Ball->flag_y:
+   * ball.flag_y:
    * 0 = up, 1 = down
    */
   
   i32 winner = 0;
 
   // move the ball
-  if(ball->flag_x == 0)
+  if(ball.flag_x == 0)
   {
-    ball->x -= ball->vx;
+    ball.x -= ball.vx;
   }
-  else if(ball->flag_x == 1)
+  else if(ball.flag_x == 1)
   {
-    ball->x += ball->vx;
+    ball.x += ball.vx;
   }
 
-  if(ball->flag_y == 0)
+  if(ball.flag_y == 0)
   {
-    ball->y -= ball->vy;
+    ball.y -= ball.vy;
   }
-  else if(ball->flag_y == 1)
+  else if(ball.flag_y == 1)
   {
-    ball->y += ball->vy;
+    ball.y += ball.vy;
   }
 
   // check if the ball is past the screen on the x axis
-  if(ball->x < 0)
+  if(ball.x < 0)
   {
-    player_two->score++;
+    player_two.score++;
 
-    if(player_two->score >= 5)
+    if(player_two.score >= 5)
     {
       winner = 2;
     }
 
-    ball->flag_x = rand() % 2;
-    ball->flag_y = rand() % 2;
-    ball->x = game_win_width / 2;
-    ball->y = game_win_height / 2;
-    ball->vx = 0.0002;
-    ball->vy = 0.0002;
-    player_one->can_add_speed = 1;
-    player_two->can_add_speed = 1;
+    ball.flag_x = rand() % 2;
+    ball.flag_y = rand() % 2;
+    ball.x = game_win_w / 2;
+    ball.y = game_win_h / 2;
+    ball.vx = 0.0002;
+    ball.vy = 0.0002;
+    player_one.can_add_speed = 1;
+    player_two.can_add_speed = 1;
   }
-  else if(ball->x > game_win_width)
+  else if(ball.x > game_win_w)
   {
-    player_one->score++;
+    player_one.score++;
 
-    if(player_one->score >= 5)
+    if(player_one.score >= 5)
     {
       winner = 1;
     }
 
-    ball->flag_x = rand() % 2;
-    ball->flag_y = rand() % 2;
-    ball->x = game_win_width / 2;
-    ball->y = game_win_height / 2;
-    ball->vx = 0.0002;
-    ball->vy = 0.0002;
-    player_one->can_add_speed = 1;
-    player_two->can_add_speed = 1;
+    ball.flag_x = rand() % 2;
+    ball.flag_y = rand() % 2;
+    ball.x = game_win_w / 2;
+    ball.y = game_win_h / 2;
+    ball.vx = 0.0002;
+    ball.vy = 0.0002;
+    player_one.can_add_speed = 1;
+    player_two.can_add_speed = 1;
   }
 
   // check if the ball is past the screen on the y axis
-  if(ball->y < 1)
+  if(ball.y < 1)
   {
-    ball->flag_y = 1;
+    ball.flag_y = 1;
   }
-  else if(ball->y > game_win_height - 1)
+  else if(ball.y > game_win_h - 1)
   {
-    ball->flag_y = 0;
+    ball.flag_y = 0;
   }
 
   // player one to ball collision
-  if(ball->x > player_one->x && ball->x < player_one->x + 1 && ball->y > player_one->y - 1 && ball->y < (player_one->y + player_one->height))
+  if(ball.x > player_one.x && ball.x < player_one.x + 1 && ball.y > player_one.y - 1 && ball.y < (player_one.y + player_one.h))
   {
     // reverse x
-    if(ball->flag_x == 1)
+    if(ball.flag_x == 1)
     {
-      ball->flag_x = 0;
+      ball.flag_x = 0;
     }
-    else if(ball->flag_x == 0)
+    else if(ball.flag_x == 0)
     {
-      ball->flag_x = 1;
+      ball.flag_x = 1;
     }
 
-    if(player_one->can_add_speed == 1)
+    if(player_one.can_add_speed == 1)
     {
       // add speed to the ball if it's below the max
-      if(ball->vx && ball->vy < 0.002)
+      if(ball.vx && ball.vy < 0.002)
       {
-        ball->vx += 0.0001;
-        ball->vy += 0.0001;
+        ball.vx += 0.0001;
+        ball.vy += 0.0001;
       }
 
-      player_one->can_add_speed = 0;
-      player_two->can_add_speed = 1;
+      player_one.can_add_speed = 0;
+      player_two.can_add_speed = 1;
     }
   }
 
   // player two to ball collision
-  if(ball->x < player_two->x && ball->x > player_two->x - 1 && ball->y > player_two->y - 1 && ball->y < (player_two->y + player_two->height))
+  if(ball.x < player_two.x && ball.x > player_two.x - 1 && ball.y > player_two.y - 1 && ball.y < (player_two.y + player_two.h))
   {
     // reverse x
-    if(ball->flag_x == 1)
-      ball->flag_x = 0;
-    else if(ball->flag_x == 0)
-      ball->flag_x = 1;
+    if(ball.flag_x == 1)
+      ball.flag_x = 0;
+    else if(ball.flag_x == 0)
+      ball.flag_x = 1;
 
-    if(player_two->can_add_speed == 1)
+    if(player_two.can_add_speed == 1)
     {
       // add speed to the ball if it's below the max
-      if(ball->vx && ball->vy < 0.002)
+      if(ball.vx && ball.vy < 0.002)
       {
-        ball->vx += 0.0001;
-        ball->vy += 0.0001;
+        ball.vx += 0.0001;
+        ball.vy += 0.0001;
       }
 
-      player_one->can_add_speed = 1;
-      player_two->can_add_speed = 0;
+      player_one.can_add_speed = 1;
+      player_two.can_add_speed = 0;
     }
   }
 
@@ -256,14 +273,14 @@ render_main_menu(WINDOW *game_win)
   wattroff(game_win, COLOR_PAIR(white_pair));
 
   // render the menu options
-  char menu_item[32] = "> ";
-  amount_to_print = 6;
+  char menu_item[16] = "> ";
+  amount_to_print = 10;
 
   // calculate the amount to print for the menu item text
-  if(current_width < 12)
+  if(current_width < 16)
   {
-    amount_to_remove = 12 - current_width;
-    amount_to_print = amount_to_print - amount_to_remove;
+    amount_to_remove = 16 - current_width;
+    amount_to_print -= amount_to_remove;
   }
 
   for(i32 i = 0; i < menu_item_amount; i++)
@@ -366,7 +383,7 @@ update_main_menu(WINDOW *game_win)
 }
 
 internal void
-render_controls(WINDOW *game_win)
+render_controls()
 {
   werase(game_win);
 
@@ -389,122 +406,91 @@ render_controls(WINDOW *game_win)
 }
 
 internal void
-render_score_window(WINDOW *score_win, i32 player_one_score, i32 player_two_score)
+render_score_window()
 {
   wattron(score_win, COLOR_PAIR(white_pair));
-  wresize(score_win, score_win_height, score_win_width);
+  wresize(score_win, score_win_h, score_win_w);
   box(score_win, 0, 0);
 
-  mvwprintw(score_win, 1, 1, "Player 1: %d", player_one_score);
-  mvwprintw(score_win, 1, 48, "Player 2: %d", player_two_score);
+  mvwprintw(score_win, 1, 1, "Player 1: %d", player_one.score);
+  mvwprintw(score_win, 1, 48, "Player 2: %d", player_two.score);
 
   wattroff(score_win, COLOR_PAIR(white_pair));
 }
 
 internal void
-update_players(player_t *player_one, player_t *player_two)
+update_players()
 {
   switch(key_pressed)
   {
     case 'q':
     {
-      running = 0;
+      game.state = state_quit;
     } break;
 
     case 'w':
     {
-      if(player_one->y > 1)
+      if(player_one.y > 1)
       {
-        player_one->y--;
+        player_one.y--;
       }
     } break;
 
     case 's':
     {
-      if((player_one->y + player_one->height) < (game_win_height - 1))
+      if((player_one.y + player_one.h) < (game_win_h - 1))
       {
-        player_one->y++;
+        player_one.y++;
       }
     } break;
 
     case KEY_UP:
     {
-      if(player_two->y > 1)
+      if(player_two.y > 1)
       {
-        player_two->y--;
+        player_two.y--;
       }
     } break;
 
     case KEY_DOWN:
     {
-      if((player_two->y + player_two->height) < (game_win_height - 1))
+      if((player_two.y + player_two.h) < (game_win_h - 1))
       {
-        player_two->y++;
+        player_two.y++;
       }
     } break;
   }
 }
 
 internal void
-render_arena(WINDOW *game_win)
+render_arena()
 {
   // corners
   wattron(game_win, COLOR_PAIR(white_pair));
   mvwaddstr(game_win, 0, 0, "+");
-  mvwaddstr(game_win, 0, game_win_width - 1, "+");
-  mvwaddstr(game_win, game_win_height - 1, 0, "+");
-  mvwaddstr(game_win, game_win_height - 1, game_win_width - 1, "+");
+  mvwaddstr(game_win, 0, game_win_w - 1, "+");
+  mvwaddstr(game_win, game_win_h - 1, 0, "+");
+  mvwaddstr(game_win, game_win_h - 1, game_win_w - 1, "+");
 
   // left / right border
-  for(i32 y = 1; y < game_win_height - 1; y++)
+  for(i32 y = 1; y < game_win_h - 1; y++)
   {
     mvwaddstr(game_win, y, 0, "|");
-    mvwaddstr(game_win, y, game_win_width - 1, "|");
+    mvwaddstr(game_win, y, game_win_w - 1, "|");
   }
 
   // top / bottom border
-  for(i32 x = 1; x < game_win_width - 1; x++)
+  for(i32 x = 1; x < game_win_w - 1; x++)
   {
     mvwaddstr(game_win, 0, x, "-");
-    mvwaddstr(game_win, game_win_height - 1, x, "-");
+    mvwaddstr(game_win, game_win_h - 1, x, "-");
   }
 
   wattroff(game_win, COLOR_PAIR(white_pair));
 }
 
-internal player_t*
-create_player(i32 x, i32 y, i32 width, i32 height)
-{
-  player_t *p = malloc(sizeof(player_t));
-
-  p->x = x;
-  p->y = y;
-  p->width = width;
-  p->height = height;
-  p->score = 0;
-  p->can_add_speed = 1;
-
-  return p;
-}
-
-internal ball_t*
-create_ball(char glyph, i32 x, i32 y, r32 vx, r32 vy)
-{
-  ball_t *b = malloc(sizeof(ball_t));
-
-  b->flag_x = rand() % 2;
-  b->flag_y = rand() % 2;
-  b->glyph = glyph;
-  b->x = x;
-  b->y = y;
-  b->vx = vx;
-  b->vy = vy;
-
-  return b;
-}
-
 internal void
-render_victory_screen(WINDOW *game_win, WINDOW *score_win, i32 winner)
+render_victory_screen(i32 winner)
 {
   nodelay(game_win, 0);
 
@@ -512,14 +498,14 @@ render_victory_screen(WINDOW *game_win, WINDOW *score_win, i32 winner)
   werase(score_win);
 
   wattron(game_win, COLOR_PAIR(white_pair));
-  mvwprintw(game_win, 5, game_win_width / 2, "Player %d wins!", winner);
+  mvwprintw(game_win, 5, game_win_w / 2, "Player %d wins!", winner);
   mvwaddstr(game_win, 10, 26, "[Q] for main menu");
   mvwaddstr(game_win, 11, 26, "[R] for a rematch");
 
   for(i32 y = 3; y < 8; y++)
   {
-    mvwaddstr(game_win, y, (game_win_width / 2) - 4, "|");
-    mvwaddstr(game_win, y, (game_win_width / 2) + 17, "|");
+    mvwaddstr(game_win, y, (game_win_w / 2) - 4, "|");
+    mvwaddstr(game_win, y, (game_win_w / 2) + 17, "|");
   }
 
   for(i32 x = 26; x < 48; x++)
@@ -538,7 +524,7 @@ render_victory_screen(WINDOW *game_win, WINDOW *score_win, i32 winner)
 
     if(input == 'Q' || input == 'q')
     {
-      gamestate = gamestate_main_menu;
+      game.state = state_main_menu;
       break;
     }
     else if(input == 'R' || input == 'r')
@@ -550,26 +536,11 @@ render_victory_screen(WINDOW *game_win, WINDOW *score_win, i32 winner)
 }
 
 internal void
-exit_game(player_t *player_one, player_t *player_two, ball_t *ball)
+run_game()
 {
-  free(player_one);
-  player_one = NULL;
-
-  free(player_two);
-  player_two = NULL;
-
-  free(ball);
-  ball = NULL;
-
-  endwin();
-}
-
-internal void
-run_game(WINDOW *game_win, WINDOW *score_win, player_t *player_one, player_t *player_two, ball_t *ball)
-{
-  while(running)
+  while(game.state != state_quit)
   {
-    if(gamestate == gamestate_main_menu)
+    if(game.state == state_main_menu)
     {
       werase(game_win);
 
@@ -579,54 +550,54 @@ run_game(WINDOW *game_win, WINDOW *score_win, player_t *player_one, player_t *pl
       if(result == 1)
       {
         nodelay(game_win, 1);
-        gamestate = gamestate_play;
+        game.state = state_play;
       }
       else if(result == 2)
       {
-        gamestate = gamestate_controls;
+        game.state = state_controls;
       }
       else if(result == 3)
       {
-        running = 0;
+        game.state = state_quit;
       }
 
       wrefresh(game_win);
     }
 
-    if(gamestate == gamestate_play)
+    if(game.state == state_play)
     {
       key_pressed = wgetch(game_win);
       werase(game_win);
       werase(score_win);
 
-      update_players(player_one, player_two);
-      i32 winner = update_ball(ball, player_one, player_two);
+      update_players();
+      i32 winner = update_ball();
 
-      render_arena(game_win);
-      render_players(game_win, player_one, player_two);
-      render_ball(game_win, ball);
-      render_score_window(score_win, player_one->score, player_two->score);
+      render_arena();
+      render_players();
+      render_ball();
+      render_score_window();
 
       if(winner)
       {
-        render_victory_screen(game_win, score_win, winner);
+        render_victory_screen(winner);
 
-        player_one->score = 0;
-        player_two->score = 0;
+        player_one.score = 0;
+        player_two.score = 0;
 
-        player_one->y = (game_win_height / 2) - 2;
-        player_two->y = (game_win_height / 2) - 2;
+        player_one.y = (game_win_h / 2) - 2;
+        player_two.y = (game_win_h / 2) - 2;
       }
 
       wrefresh(game_win);
       wrefresh(score_win);
     }
 
-    if(gamestate == gamestate_controls)
+    if(game.state == state_controls)
     {
       render_controls(game_win);
 
-      gamestate = gamestate_main_menu;
+      game.state = state_main_menu;
     }
   }
 }
@@ -667,15 +638,8 @@ init_game()
   init_pair(cyan_pair, COLOR_CYAN, COLOR_CYAN);
   init_pair(white_pair, COLOR_WHITE, COLOR_CYAN);
 
-  return 1;
-}
-
-i32
-main()
-{
-  // create our main windows
-  WINDOW *game_win = newwin(game_win_height, game_win_width, game_win_start_y, game_win_start_x);
-  WINDOW *score_win = newwin(score_win_height, score_win_width, 0, 0);
+  game_win = newwin(game_win_h, game_win_w, game_win_y, game_win_x);
+  score_win = newwin(score_win_h, score_win_w, 0, 0);
 
   wbkgd(stdscr, COLOR_PAIR(cyan_pair));
   wbkgd(game_win, COLOR_PAIR(cyan_pair));
@@ -685,18 +649,37 @@ main()
   nodelay(game_win, 0); // getch will block execution
   wrefresh(stdscr);
 
-  // create players and the ball
-  player_t *player_one = create_player(2, 8, 1, 4);
-  player_t *player_two = create_player(57, 8, 1, 4);
-  ball_t *ball = create_ball('O', game_win_width / 2, game_win_height / 2, 0.0002, 0.0002);
+  player_one.x = 2;
+  player_one.y = 8;
+  player_one.w = 1;
+  player_one.h = 4;
+  player_one.can_add_speed = 1;
 
+  player_two.x = 57;
+  player_two.y = 8;
+  player_two.w = 1;
+  player_two.h = 4;
+  player_two.can_add_speed = 1;
+
+  ball.glyph = 'O';
+  ball.x = game_win_w / 2;
+  ball.y = game_win_y / 2;
+  ball.vx = 0.0002;
+  ball.vy = 0.0002;
+
+  return 1;
+}
+
+i32
+main()
+{
   if(init_game())
   {
-    run_game(game_win, score_win, player_one, player_two, ball);
-    exit_game(player_one, player_two, ball);
+    run_game();
+    endwin();
     return EXIT_SUCCESS;
   }
 
-  exit_game(player_one, player_two, ball);
+  endwin();
   return EXIT_FAILURE;
 }
